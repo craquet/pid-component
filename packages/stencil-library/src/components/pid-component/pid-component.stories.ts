957382ebc9398b682902dcd367656b4819e094a4
Maximilian Inckmann <maximilian.inckmann@kit.edu>
2026-05-11 11:29:01 +0200

    bugfixes
    Signed-off-by: Maximilian Inckmann <maximilian.inckmann@kit.edu>

--- packages/stencil-library/src/components/pid-component/pid-component.stories.ts
+++ packages/stencil-library/src/components/pid-component/pid-component.stories.ts
@@ -162,7 +162,7 @@ const meta: Meta = {
   },
 };
 
-const textDecorator = (story: () => unknown) =>
+const _textDecorator = (story: () => unknown) =>
   html`<p class="items-center align-middle">
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
     ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
--- packages/stencil-library/src/rendererModules/RORType/ROR.stories.ts
+++ packages/stencil-library/src/rendererModules/RORType/ROR.stories.ts
@@ -1,4 +1,4 @@
-import { html } from 'lit';
+import { _html } from 'lit';
 import { Meta, StoryObj } from '@storybook/web-components-vite';
 import { ROR_examples } from '../../../../../examples';
 
